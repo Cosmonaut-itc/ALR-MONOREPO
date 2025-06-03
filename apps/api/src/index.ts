ac2f9d21a3a3ab3b4c26c7535760b72bf416aae2
cosmonaut-itc <felixddhs@outlook.com>
2025-06-03 14:39:26 -0600

    feat: add API key management and enhance authentication flow

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,15 +1,47 @@
 import { Hono } from "hono";
+import { auth } from "./lib/auth";
+import { serve } from "@hono/node-server";
+import { cors } from "hono/cors";
+import { z } from "zod";
+import { zValidator } from "@hono/zod-validator";
 
-const app = new Hono();
+const app = new Hono<{
+	Variables: {
+		user: typeof auth.$Infer.Session.user | null;
+		session: typeof auth.$Infer.Session.session | null;
+	};
+}>();
 
-app.get("/", (c) => {
-	return c.text("Hello Hono!");
+app.use(
+	"/api/auth/*", // or replace with "*" to enable cors for all routes
+	cors({
+		origin: "http://localhost:3001", // replace with your origin
+		allowHeaders: ["Content-Type", "Authorization"],
+		allowMethods: ["POST", "GET", "OPTIONS"],
+		exposeHeaders: ["Content-Length"],
+		maxAge: 600,
+		credentials: true,
+	}),
+);
+
+app.use("*", async (c, next) => {
+	const session = await auth.api.getSession({ headers: c.req.raw.headers });
+
+	if (!session) {
+		c.set("user", null);
+		c.set("session", null);
+		return next();
+	}
+
+	c.set("user", session.user);
+	c.set("session", session.session);
+	return next();
 });
 
-app.get("/auth", (c) => {
-	return c.json({
-		message: "Hello Hono Auth!",
-	});
+app.on(["POST", "GET"], "/api/auth/*", (c) => {
+	return auth.handler(c.req.raw);
 });
+
+serve(app);
 
 export default app;
